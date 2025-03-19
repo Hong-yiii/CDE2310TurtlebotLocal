2e62155c953ef7206243ce192bce995399fe7f35
Hong-yiii <hongyilin.mail@gmail.com>
2025-03-19 16:54:49 +0800

    commented out package

--- turtlebot3/turtlebot3_bringup/launch/robot.launch.py
+++ turtlebot3/turtlebot3_bringup/launch/robot.launch.py
@@ -92,11 +92,11 @@ def generate_launch_description():
             arguments=['-i', usb_port],
             output='screen'),
 
-        Node(
-            package='turtlebot3_flywheels_and_Temperature',
-            executable='flywheels_and_Temperature',
-            output='screen'
-        ),
+        # Node(
+        #     package='turtlebot3_flywheels_and_Temperature',
+        #     executable='flywheels_and_Temperature',
+        #     output='screen'
+        # ),
 
         Node(
             package='nav2_controller',
