669a75d77e2e63b2faf89ea72490b8c5924b72b7
hongyiii <hongyilin.mail@gmail.com>
2025-03-19 06:10:15 +0000

    Update amg8833_sensor: added, modified, and deleted files

--- amg8833_sensor/scripts/amg8833_publisher.py
+++ amg8833_sensor/scripts/amg8833_publisher.py
@@ -11,18 +11,18 @@ class AMG8833Publisher(Node):
     def __init__(self):
         super().__init__('amg8833_publisher')
         
-       
+        
         self.publisher_ = self.create_publisher(Float32MultiArray, 'temperature_sensor', 10)
         
         
         self.timer = self.create_timer(1.0, self.publish_data)
 
-        # Initialize I2C and AMG8833 sensor
+     
         i2c_bus = busio.I2C(board.SCL, board.SDA)
         self.sensor = adafruit_amg88xx.AMG88XX(i2c_bus, address=0x69)  # Default I2C address
 
     def publish_data(self):
-        # Read temperature data from sensor
+        # Read temperature 
         temp_data = Float32MultiArray()
         temp_data.data = [temp for row in self.sensor.pixels for temp in row]  # Flatten 8×8 array
         
--- amg8833_sensor/setup.py
+++ amg8833_sensor/setup.py
@@ -1,24 +1,25 @@
-from setuptools import setup
-import os
-from glob import glob
+from setuptools import find_packages, setup
 
 package_name = 'amg8833_sensor'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
-    maintainer_email='your@email.com',
-    description='AMG8833 ROS 2 Publisher',
-    license='Apache-2.0',
+    maintainer_email='hongyilin.mail@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'amg8833_publisher = amg8833_sensor.amg8833_publisher:main'
         ],
     },
 )
-
