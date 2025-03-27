3f3e87f321861408796a894cf43f8002eab9c13f
zhengboon <e1399019@u.nus.edu>
2025-03-27 19:38:29 +0800

    Update flywheel_node.py

--- turtlebot3/turtlebot3_flywheels_and_temp/src/nodes/flywheel_node.py
+++ turtlebot3/turtlebot3_flywheels_and_temp/src/nodes/flywheel_node.py
@@ -3,33 +3,37 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 import board
 import time
+from std_msgs.msg import String
+
 
 class FlywheelSubscriber(Node):
+
     def __init__(self):
         super().__init__('flywheel_subscriber')
-        self.publisher_ = self.create_publisher(Float32, '/temp', 10)
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-        timer_period = 1.0  # Publish every 1 second
-        self.timer = self.create_timer(timer_period, self.publish_temp)
-        self.get_logger().info('Temperature Publisher Started')
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
 
-    def publish_temp(self):
-        try:
-            temp_matrix = self.sensor.pixels
-            avg_temp = sum([sum(row) for row in temp_matrix]) / 64  # Average of 8x8 matrix
-            msg = Float32()
-            msg.data = avg_temp
-            self.publisher_.publish(msg)
-            self.get_logger().info(f'Published temperature: {avg_temp:.2f}°C')
-        except Exception as e:
-            self.get_logger().error(f"Failed to read from sensor: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
-    node = FlywheelSubscriber()
-    rclpy.spin(node)
-    node.destroy_node()
+
+    flywheel_subscriber = FlywheelSubscriber()
+
+    rclpy.spin(flywheel_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    flywheel_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
