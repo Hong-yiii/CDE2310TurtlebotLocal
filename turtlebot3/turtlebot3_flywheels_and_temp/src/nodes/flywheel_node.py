a6a07e35280425fb576a785db5317f1e9682eb10
zhengboon <e1399019@u.nus.edu>
2025-03-27 20:05:54 +0800

    Update flywheel_node.py

--- turtlebot3/turtlebot3_flywheels_and_temp/src/nodes/flywheel_node.py
+++ turtlebot3/turtlebot3_flywheels_and_temp/src/nodes/flywheel_node.py
@@ -1,23 +1,20 @@
 import rclpy
 from rclpy.node import Node
-import board
-import time
 from std_msgs.msg import Int32
 import RPi.GPIO as GPIO
 
-
 class FlywheelSubscriber(Node):
 
-    
     def __init__(self):
         super().__init__('flywheel_subscriber')
         self.subscription = self.create_subscription(
-            Int,
+            Int32,
             'topic',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-            # GPIO Setup
+
+        # GPIO Setup
         self.ESC_PIN = 18
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.ESC_PIN, GPIO.OUT)
@@ -25,33 +22,32 @@ class FlywheelSubscriber(Node):
         self.pwm.start(0)
 
     def listener_callback(self, msg):
-        #self.get_logger().info('I heard: "%s"' % msg.data)
-        #insert more code below
         throttle = msg.data
         if 0 <= throttle <= 2000:
-            duty_cycle = (throttle / 20000) * 100
+            duty_cycle = 5 + (throttle / 2000) * 5
             self.pwm.ChangeDutyCycle(duty_cycle)
             self.get_logger().info(f'Throttle set to {throttle} µs')
         else:
             self.get_logger().warn('Throttle out of range (0–2000 µs)')
 
+    def destroy_node(self):
+        self.pwm.stop()
+        GPIO.cleanup()
+        super().destroy_node()
+
 
 def main(args=None):
     rclpy.init(args=args)
-
     flywheel_subscriber = FlywheelSubscriber()
 
-    rclpy.spin(flywheel_subscriber)
+    try:
+        rclpy.spin(flywheel_subscriber)
+    except KeyboardInterrupt:
+        pass
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     flywheel_subscriber.destroy_node()
     rclpy.shutdown()
-def destroy_node(self):
-    self.pwm.stop()
-    GPIO.cleanup()
-    super().destroy_node()
+
 
 if __name__ == '__main__':
     main()
