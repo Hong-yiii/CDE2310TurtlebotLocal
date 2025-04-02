ee1b48e100deb7cdf8aafb106d14a37ad8e240a2
Hong-yiii <hongyilin.mail@gmail.com>
2025-04-02 14:41:42 +0800

    colcon attempt

--- turtlebot3/turtlebot3_flywheels_and_temp/setup.py
+++ turtlebot3/turtlebot3_flywheels_and_temp/setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'turtlebot3_flywheels_and_temp'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        ('share/' + package_name + '/launch', ['launch/bringup.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
-    maintainer_email='you@example.com',
-    description='Bringup for AMG8833 and flywheels',
+    maintainer_email='your_email@example.com',
+    description='Example bringup for flywheels and AMG8833 temperature sensor',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'amg8833_publisher = turtlebot3_flywheels_and_temp.amg8833_publisher:main',
+            'flywheel_node = turtlebot3_flywheels_and_temp.flywheel_node:main',
         ],
     },
 )
