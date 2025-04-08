35762b0c65cc68480375b007219bda97ed14c281
Hong-yiii <hongyilin.mail@gmail.com>
2025-04-08 18:30:13 +0800

    Updated servo timings

--- turtlebot3/turtlebot3_flywheels_and_temp/turtlebot3_flywheels_and_temp/flywheel_node.py
+++ turtlebot3/turtlebot3_flywheels_and_temp/turtlebot3_flywheels_and_temp/flywheel_node.py
@@ -1,6 +1,3 @@
-#flywheel = receive "50"
-#flywheel_status = "Flywheel sequence complete"
-#use those 2 topics
 from std_msgs.msg import String
 from time import sleep
 import rclpy
@@ -22,63 +19,61 @@ class FlywheelSubscriber(Node):
 
         # GPIO Setup
         self.ESC_PIN = 18
-        self.SERVO_PIN = 17
+        self.SERVO_PIN = 21
         GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.SERVO_PIN, GPIO.OUT)
         GPIO.setup(self.ESC_PIN, GPIO.OUT)
         self.pwm = GPIO.PWM(self.ESC_PIN, 50)  # 50Hz for ESC
         self.pwm.start(0)
-        self.my_servo = Servo(self.SERVO_PIN, min_pulse_width=0.9/1000, max_pulse_width=2.1/1000, frame_width=20/1000)
-        self.my_servo.value = -0.26
+        self.p = GPIO.PWM(self.SERVO_PIN, 50)  # 50 Hz frequency
+        self.p.start(7.5)  # 7.5% duty cycle -> 90 degrees
         print("should be not moving")
         duty_cycle = 30*0.1
         self.pwm.ChangeDutyCycle(duty_cycle)
+        self.p.ChangeDutyCycle(0)
     def listener_callback(self, msg):
+        global p #ayo why this broken
         def calibrate_stop():
-            self.my_servo.value = -0.26  # Stop (neutral)
+            #self.my_servo.value = None  # Stop (neutral)
             print("Calibrating Stop Position...")
-
+            #GPIO.output(self.SERVO_POWER, GPIO.HIGH)
         def move_forward():
-            self.my_servo.value = 1  # Full forward
+            duty = 90 / 18 + 2.5
+            self.p.ChangeDutyCycle(duty)
+            sleep(0.5)  # Allow servo time to reach position
+            self.p.ChangeDutyCycle(0)  # Reset PWM to prevent jitter
             print("Moving Forward...")
+            #GPIO.output(self.SERVO_POWER, GPIO.LOW)
 
         def move_reverse():
-            self.my_servo.value = -1  # Full reverse
+            duty = 65 / 18 + 2.5
+            self.p.ChangeDutyCycle(duty)
+            sleep(0.5)  # Allow servo time to reach position
+            self.p.ChangeDutyCycle(0)  # Reset PWM to prevent jitter
             print("Moving Reverse...")
+            #GPIO.output(self.SERVO_POWER, GPIO.LOW)
         throttle = msg.data
         if throttle == 50:
                 #move_reverse()
                 #sleep(0.8)
+                #throttle = 55
                 duty_cycle = throttle*0.1
                 self.pwm.ChangeDutyCycle(duty_cycle)
                 self.get_logger().info(f'Throttle set to {throttle}%')
                 sleep(2)
                 move_forward()
-                sleep(0.85)
                 #first ball shot
-                calibrate_stop()
-                sleep(0.5)
-                move_reverse()
-                sleep(0.8)
-                calibrate_stop()
-                sleep(1.85)
-                move_forward()
-                sleep(0.85)
-                #second ball shot
-                calibrate_stop()
-                sleep(0.2)
-                move_reverse()
-                sleep(0.8)
-                calibrate_stop()
-                sleep(0.15)
-                move_forward()
-                sleep(0.85)
+                sleep(1)
+                move_reverse() #0.5
+                sleep(3) #1.5
+                move_forward() #2
+                #second ball shot 0
+                move_reverse() #0.5
+                sleep(1)#3.5
+                move_forward() #4
                 #third ball shot
-                calibrate_stop()
                 sleep(0.5)
                 move_reverse()
-                sleep(0.8)
-                calibrate_stop()
-                sleep(0.5)
                 #send message to say this shit done
                 duty_cycle = 0
                 throttle = 0
@@ -86,6 +81,7 @@ class FlywheelSubscriber(Node):
                 self.get_logger().info(f'Throttle set to {throttle}%')
                 done_msg = String()
                 done_msg.data = "Flywheel sequence complete"
+                self.publisher_.publish(done_msg)
                 self.get_logger().info("Published status: Flywheel sequence complete")
 
         else:
